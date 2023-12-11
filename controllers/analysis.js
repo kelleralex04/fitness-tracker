3e4da96c0ad44f164508eab796d4d9794be9340d
Alex Keller <kelleralex04@gmail.com>
2023-12-10 22:50:05 -0700

    adds rexercises to analysis index page, routing to analysis show page

--- controllers/analysis.js
+++ controllers/analysis.js
@@ -2,9 +2,61 @@ const Exercise = require('../models/exercise');
 const Category = require('../models/category');
 
 module.exports = {
-    index
+    index,
+    show
 };
 
-function index(req, res) {
-    res.render('analysis/index', { title: 'Analysis' })
+async function findUserExercises(req, res) {
+    const categories = [];
+    const exercises = [];
+    for (const c of req.user.category) {
+        categories.push(await Category.findById(c));
+    };
+    categories.sort(function(a,b) {
+        if (a.name.toUpperCase() < b.name.toUpperCase()) {
+            return -1;
+        };
+        if (a.name.toUpperCase() > b.name.toUpperCase()) {
+            return 1;
+        };
+        return 0
+    });
+    for (const e of req.user.exercise) {
+        let categoryCheck = false;
+        for (const c of categories) {
+            if (c.exercise.includes(e._id)) {
+                categoryCheck = true;
+                break;
+            };
+        };
+        if (!categoryCheck) {
+            req.user.exercise.remove(e);
+            await req.user.save();
+        }
+        exercises.push(await Exercise.findById(e));
+    };
+    return { categories: categories, exercises: exercises }
+};
+
+async function index(req, res) {
+    const categories = (await findUserExercises(req, res)).categories;
+    const exercises = (await findUserExercises(req, res)).exercises;
+    for (let i = 0; i < categories.length; i++) {
+        let categoryCheck = false;
+        for (e of exercises) {
+            if (categories[i].exercise.includes(e._id)) {
+                categoryCheck = true;
+                break;
+            };
+        };
+        if (!categoryCheck) {
+            categories.splice(i, 1);
+        };
+    };
+    res.render('analysis/index', { title: 'Analysis', categories, exercises })
+}
+
+async function show(req, res) {
+    const exercise = await Exercise.findById(req.params.id);
+    res.render('analysis/show', { title: exercise.name, exercise })
 }
--- routes/analysis.js
+++ routes/analysis.js
@@ -4,5 +4,6 @@ const analysisCtrl = require('../controllers/analysis');
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get('/', ensureLoggedIn, analysisCtrl.index);
+router.get('/:id', ensureLoggedIn, analysisCtrl.show);
 
 module.exports = router;
