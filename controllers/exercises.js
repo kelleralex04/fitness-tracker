4d573f4482eda5e2f6cf3cb156026188e944e6a7
Alex Keller <kelleralex04@gmail.com>
2023-12-10 21:57:37 -0700

    adds default exercises for new users

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -151,7 +151,7 @@ async function update(req, res) {
             await newCategory[0].save();
         };
         await Exercise.findOneAndUpdate({ _id: req.params.id }, req.body);
-        res.redirect('/exercises');
+        res.redirect(`/exercises/${req.params.id}`);
     } catch(err) {
         console.log(err);
         res.redirect('/exercises');
